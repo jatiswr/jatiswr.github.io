8c3b76c7bb0cb4312759a3554f43dc7d004f4c80
Jati Iswara <premaiswara22@gmail.com>
2024-12-18 10:02:31 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         poppins: ["Poppins"],
         raleway: ["Raleway"],
       },
+      boxShadow: {
+        neon: "0px 0px 17px 6px rgba(34, 211, 238, 1)",
+        neon2: "0px 0px 6px 3px rgba(34, 211, 238, 1)",
+      },
     },
   },
   plugins: [],
